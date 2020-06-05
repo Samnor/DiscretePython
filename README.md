c6d45c2c180f14dd07d03e684153fa0709d231b0
Samuel Norling <norling.samuel@gmail.com>
2020-06-05 14:03:29 +0200

    Pylama fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 DiscretePython
+
+The goal of this project is to try concepts from the book Effective Coding. Especially I want to write code that adheres to the PEP8. Developed with VSCode and Pylama as linter.
--- group_generators.py
+++ group_generators.py
@@ -1,24 +1,28 @@
 import itertools
 from Group import Group
 
+
 def test_z_addition_group():
     n = 8
     steps = 20
     start_element = 5
 
-    z_n_group = Group(group_set=range(0,n), bin_operation=lambda x,y: (x + y) % n)
+    z_n_group = Group(group_set=range(0, n),
+                      bin_operation=lambda x, y: (x + y) % n)
     cycle_steps = z_n_group.get_element_cycle_steps(start_element, steps)
     for step in cycle_steps:
         if(step == start_element):
             print()
         print(f"{step}", end=f" ")
 
+
 def test_z_multiplication_group():
     n = 9
     steps = 20
-    start_element = 4
+    start_element = 5
 
-    z_n_group = Group(group_set=range(0,n), bin_operation=lambda x,y: (x * y) % n)
+    z_n_group = Group(group_set=range(0, n),
+                      bin_operation=lambda x, y: (x * y) % n)
     cycle_steps = z_n_group.get_element_cycle_steps(start_element, steps)
     for step in cycle_steps:
         if(step == start_element):
@@ -27,6 +31,4 @@ def test_z_multiplication_group():
 
 
 if __name__ == "__main__":
-    #test_z_n_group()
     test_z_multiplication_group()
-
